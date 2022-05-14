bb409675d09766568e3240a1256156faaac8e545
Quentin Le Bacquer <quentinnomade@mail.com>
2022-05-14 11:51:50 +0200

    GUI

--- Morpion.py
+++ Morpion.py
@@ -1,4 +1,4 @@
-grille = [[0,0,0], [0,0,0], [0,0,0]]
+from tkinter import *  
 
 def round(grille, x, y, joueur):
 
@@ -38,37 +38,94 @@ def end(grille):
 
     return -1
 
-print(" ".join(str(grille[0])))
-print(" ".join(str(grille[1])))
-print(" ".join(str(grille[2])))
+def jouer(grille, x, y, boutons) :
 
-joueur = 1   
+    compteur = 0
 
-while end(grille) == 0:
-
-    print("Au tour du joueur "+str(joueur))
-
-    x = int(input("Sur quelle ligne voulez-vous jouer ??? "))
-    y = int(input("Sur quelle colonne voulez-vous jouer ??? "))
-
-    while not round(grille, x, y, joueur):
-        print("La case est déja prise ou pas dans la grille, merci de bouger.")
-        x = int(input("Sur quelle ligne voulez-vous jouer ??? "))
-        y = int(input("Sur quelle colonne voulez-vous jouer ??? "))
-
-
-    print(" ".join(str(grille[0])))
-    print(" ".join(str(grille[1])))
-    print(" ".join(str(grille[2])))
-
-    if joueur == 1:
-        joueur = 2 
-
-    elif joueur == 2:
+    for x1 in range(len(grille)) :
+        for y1 in range(len(grille[x])) :
+            if grille[x1][y1] == 0:
+                compteur += 1
+    
+    if compteur % 2 == 1 :
         joueur = 1
+    else :
+        joueur = 2
 
-gagnant = end(grille)
-if gagnant == -1:
-    print("Pas de gagnant, redémarrez la partie ")
-else :
-    print("Le joueur "+str(gagnant)+" a gagné ! Bravo !")
+    reply = round(grille, x, y, joueur)
+
+    if joueur == 1 and reply:
+        boutons[3 * x + y]['text'] = 'X'
+
+    elif joueur == 2 and reply:
+        boutons[3 * x + y]['text'] = 'O'
+
+    gagnant = end(grille)
+
+    if gagnant == 0:
+        return
+
+    elif gagnant == -1:
+
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        text_gagnant = Label(window, text = "Bizzare")
+        text_gagnant.pack()
+
+        btn = Button(window, text ="Recommencer", command = begin)
+        btn.pack()
+
+    else:
+
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        text_gagnant = Label(window, text = "Le gagnant c le : "+str(gagnant))
+        text_gagnant.pack()
+
+        btn = Button(window, text ="Recommencer", command = begin)
+        btn.pack()
+
+def begin() :
+
+    for widget in window.winfo_children():
+            widget.destroy()
+
+    btn11 = Button(window, text =" ", command = lambda : jouer(grille, 0, 0, boutons), height = 15, width = 20)
+    btn11.grid(row = 0, column = 0)
+
+    btn12 = Button(window, text =" ", command = lambda : jouer(grille, 0, 1, boutons), height = 15, width = 20)
+    btn12.grid(row = 0, column = 1)
+
+    btn13 = Button(window, text =" ", command = lambda : jouer(grille, 0, 2, boutons), height = 15, width = 20)
+    btn13.grid(row = 0, column = 2)
+
+    btn21 = Button(window, text =" ", command = lambda : jouer(grille, 1, 0, boutons), height = 15, width = 20)
+    btn21.grid(row = 1, column = 0)
+
+    btn22 = Button(window, text =" ", command = lambda : jouer(grille, 1, 1, boutons), height = 15, width = 20)
+    btn22.grid(row = 1, column = 1)
+
+    btn23 = Button(window, text =" ", command = lambda : jouer(grille, 1, 2, boutons), height = 15, width = 20)
+    btn23.grid(row = 1, column = 2)
+
+    btn31 = Button(window, text =" ", command = lambda : jouer(grille, 2, 0, boutons), height = 15, width = 20)
+    btn31.grid(row = 2, column = 0)
+
+    btn32 = Button(window, text =" ", command = lambda : jouer(grille, 2, 1, boutons), height = 15, width = 20)
+    btn32.grid(row = 2, column = 1)
+
+    btn33 = Button(window, text =" ", command = lambda : jouer(grille, 2, 2, boutons), height = 15, width = 20)
+    btn33.grid(row = 2, column = 2)
+
+    boutons = [btn11, btn12, btn13, btn21, btn22, btn23, btn31, btn32, btn33]
+
+    grille = [[0,0,0], [0,0,0], [0,0,0]]
+
+window = Tk()
+window.attributes('-fullscreen', True) 
+
+begin()
+
+window.mainloop()
